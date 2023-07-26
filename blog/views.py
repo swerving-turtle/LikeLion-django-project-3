e685febf0c416308ff732a8a67240769d4711334
Keonwoo Moon <swervingturtle@gmail.com>
2023-07-26 15:50:49 +0900

    post_list를 CBV로 변경

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 
 from blog.models import Post
 
 
 # Create your views here.
-def post_list(request):
-    posts = Post.objects.filter(status=Post.Status.PUBLISHED)
-    return render(request, template_name='blog/post_list.html', context={'posts': posts})
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    paginate_by = 3
+    def get_queryset(self):
+        return Post.objects.filter(status=Post.Status.PUBLISHED)
 def post_detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     return render(request, template_name='blog/post_detail.html', context={'post': post})
