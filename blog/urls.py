8e4b5ba17db5237c9b0688721f609d750e436946
Keonwoo Moon <swervingturtle@gmail.com>
2023-07-27 13:12:42 +0900

    url 뒤에 / 추가

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
-    path('<int:post_id>', views.post_detail, name='post_detail'),
-    path('<int:post_id>/share', views.post_share, name='post_share'),
+    path('<int:post_id>/', views.post_detail, name='post_detail'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
 ]
