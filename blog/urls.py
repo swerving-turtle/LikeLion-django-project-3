5a175bc7a9c1d87840e950da3ef7fb777d887307
Keonwoo Moon <swervingturtle@gmail.com>
2023-07-27 10:36:54 +0900

    이메일 공유 뷰, 템플릿 추가

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
     path('<int:post_id>', views.post_detail, name='post_detail'),
+    path('<int:post_id>/share', views.post_share, name='post_share'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
-
 from blog.models import Post
-
+from django.core.mail import send_mail
+from blog.forms import EmailPostForm
 
 # Create your views here.
 class PostListView(ListView):
@@ -15,3 +15,21 @@ class PostListView(ListView):
 def post_detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     return render(request, template_name='blog/post_detail.html', context={'post': post})
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    sent = False
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{data['name']} 님이 {post.title}을(를) 추천합니다."
+            message = f"{post.title}을 {post_url}에서 읽어보세요.\n\n" \
+                      f"{data['name']}님의 의견: {data['comments']}"
+            send_mail(subject, message, '', [data['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+
+    return render(request, 'blog/post_share.html', {'post': post, 'form': form, 'sent': sent})
